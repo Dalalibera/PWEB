ddcf0519c3d2da5f5aebdccceab691ca0dc9496e
Giovanne <giovannedalalibera@hotmail.com>
2021-04-07 21:10:10 -0300

    Adiciona validação das entradas

--- Atividade7/script.js
+++ Atividade7/script.js
@@ -1,7 +1,26 @@
 const maior = (a, b, c) => a > b && a > c ? a : b > c ? b : c;
 
-const a = parseFloat(prompt("Informe o primeiro número"));
-const b = parseFloat(prompt("Informe o segundo número"));
-const c = parseFloat(prompt("Informe o terceiro número"));
+let a, b, c;
+
+do{
+    a = parseFloat(prompt("Informe o primeiro número:"));
+    if (isNaN(a)){
+        alert("Entrada inválida!");
+    }
+}while(isNaN(a))
+
+do{
+    b = parseFloat(prompt("Informe o primeiro número:"));
+    if (isNaN(b)){
+        alert("Entrada inválida!");
+    }
+}while(isNaN(b))
+
+do{
+    c = parseFloat(prompt("Informe o primeiro número:"));
+    if (isNaN(c)){
+        alert("Entrada inválida!");
+    }
+}while(isNaN(c))
 
 alert(`O maior número informado foi ${maior(a, b, c)}`);
